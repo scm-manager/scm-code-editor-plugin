01679168e165f44ee3fac18c222824bdadb594b6
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-10-22 09:09:44 +0200

    Export supported an array of supported languages

--- scripts/gen-languages.js
+++ scripts/gen-languages.js
@@ -21,6 +21,18 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+// @ts-nocheck
+const fs = require("fs");
+const path = require("path");
+const prettier = require("prettier");
+const modeDirectory = path.resolve(__dirname, "..", "node_modules", "ace-builds", "src-noconflict");
+const targetFile = path.resolve(__dirname, "..", "src", "main", "js", "languages.ts");
 
-export { default as CodeEditor } from "./components/CodeEditor";
-export { default as languages } from "./languages";
+const modes = fs
+  .readdirSync(modeDirectory)
+  .filter(file => file.indexOf("mode-") === 0)
+  .map(file => '"' + file.substring(5, file.length - 3) + '"')
+  .join(",");
+
+const content = prettier.format(`const languages = [${modes}]; export default languages;`);
+fs.writeFileSync(targetFile, content);
